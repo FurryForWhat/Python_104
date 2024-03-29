b4f1f8b80b149ee06aa028525cc8c5ea581d2357
furry33 <60417722+FurryForWhat@users.noreply.github.com>
2024-03-29 11:04:54 +0630

    Add files via upload

---
+++ Day7_1.py
@@ -0,0 +1,4 @@
+a = '18'
+b = 1
+a = int(a)
+print(a + b) #string concatenate
---
+++ Day7_2.py
@@ -0,0 +1,6 @@
+num_one = 'c'
+num_two = 'a'
+num_three = 'r'
+
+print(num_one + num_two + num_three) #car
+print(num_two + num_three + num_one) #arc
