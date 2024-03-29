5dd21fabc7b01b4a2d18c794567d4bbaa60b3259
furry33 <60417722+FurryForWhat@users.noreply.github.com>
2024-03-29 11:10:53 +0630

    Add files via upload

---
+++ Day8_1.py
@@ -0,0 +1,21 @@
+first = [1,2,3,'Hi','Mg Mg',[12,'21',3.14],True]
+second = 'Let me3know if you want me to generate a specific set of questions at a particular difficulty level!'
+third = 'Hello'
+# print(len(first))
+# print(len(second))
+
+# print(first)
+# print(len(first))
+# print(first[5][0])
+
+# print(len(second))
+# print(second[5]+second[6])
+
+print(first[0] + first[1])
+# print(first[2] + first[3])
+print(first[3] + first[4])
+# print(first[4] + first[5])
+# print(first[5] + first[0])
+print(first[0] + first[5][2])
+print(first[0] + int(first[5][1]))
+print(first[0] + first[5][0])
---
+++ Day8_2.py
@@ -0,0 +1,11 @@
+a = 3
+b = 4
+after_add = a + b
+after_subtract = a - b
+after_multiple = a * b
+after_divide = a / b
+after_divide_1 = a // b
+after_divide_2 = a % b
+    
+print(f'ပေါင်းလဒ်:{after_add} , နှုတ်လဒ်:{after_subtract}, မြှောက်လဒ်: {after_multiple}, စားလဒ်အတိအကျ: {after_divide}')
+print(f'စားလဒ်: {after_divide_1}, အကြွင်း: {after_divide_2}')
